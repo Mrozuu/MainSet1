67dbaff1d82674fce136ef637b768bf3d02deed9
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-07-17 05:28:21 +0200

    Updates

---
+++ precache-manifest.7f3983321bc9f9d550d8ef1b64ad0ec0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3a7b52a22d1b11622024ac065bca5e",
+    "url": "/MainSet1/index.html"
+  },
+  {
+    "revision": "e1a009b06a7d64ae072c",
+    "url": "/MainSet1/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "cbf502dacd6cd1470027",
+    "url": "/MainSet1/static/css/main.92ef88d5.chunk.css"
+  },
+  {
+    "revision": "e1a009b06a7d64ae072c",
+    "url": "/MainSet1/static/js/2.318d2f01.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/MainSet1/static/js/2.318d2f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf502dacd6cd1470027",
+    "url": "/MainSet1/static/js/main.07af4ae6.chunk.js"
+  },
+  {
+    "revision": "4b8ecbe8fcfe4f4f96f2",
+    "url": "/MainSet1/static/js/runtime-main.931be7f8.js"
+  },
+  {
+    "revision": "2bd0aac80d0b0df1ba093550ff5d4a1b",
+    "url": "/MainSet1/static/media/arrow.2bd0aac8.svg"
+  },
+  {
+    "revision": "c4b611e6fdb485b2ad56f32f8b535c3b",
+    "url": "/MainSet1/static/media/dezerek.c4b611e6.svg"
+  },
+  {
+    "revision": "29460a9685585dd59dd68061217b2439",
+    "url": "/MainSet1/static/media/fontello.29460a96.woff"
+  },
+  {
+    "revision": "7bbf6b669c06620dd364c27ae72758f2",
+    "url": "/MainSet1/static/media/fontello.7bbf6b66.svg"
+  },
+  {
+    "revision": "c03dbfe4fd4ea6bac68c79c8c954c1bf",
+    "url": "/MainSet1/static/media/fontello.c03dbfe4.ttf"
+  },
+  {
+    "revision": "e135e28a93f6ee9dd77e547323a37c25",
+    "url": "/MainSet1/static/media/fontello.e135e28a.woff2"
+  },
+  {
+    "revision": "f7e1cc1b4f5fcf7b71068eb6ac60d70f",
+    "url": "/MainSet1/static/media/fontello.f7e1cc1b.eot"
+  },
+  {
+    "revision": "30a3026cf0a4b477ef11585cf3247bb1",
+    "url": "/MainSet1/static/media/foodloosenapis.30a3026c.svg"
+  },
+  {
+    "revision": "7afa2917b6f1bd74ff716f4428a4dbe8",
+    "url": "/MainSet1/static/media/mcdalenaglowa1.7afa2917.svg"
+  },
+  {
+    "revision": "6e8bcc82790de60ce413cc5cfbb38fd1",
+    "url": "/MainSet1/static/media/migzer.6e8bcc82.svg"
+  },
+  {
+    "revision": "a1c6069d3c64fac581a38356553ae60e",
+    "url": "/MainSet1/static/media/pic1.a1c6069d.jpg"
+  },
+  {
+    "revision": "b2bbf268620611720fe0acc3d0f96aaf",
+    "url": "/MainSet1/static/media/pic1.b2bbf268.jpg"
+  },
+  {
+    "revision": "373c1b7a3ef996d0c6b39a4ae5988755",
+    "url": "/MainSet1/static/media/pic2.373c1b7a.jpg"
+  },
+  {
+    "revision": "c92647c3fd8c7b60a0d5e8cc26a6ffc7",
+    "url": "/MainSet1/static/media/pic2.c92647c3.jpg"
+  },
+  {
+    "revision": "75d2747096bbb2ea598850b17f79f2b2",
+    "url": "/MainSet1/static/media/pic3.75d27470.jpg"
+  },
+  {
+    "revision": "c4229c43b4ba26024da0f99c43305444",
+    "url": "/MainSet1/static/media/pic3.c4229c43.jpg"
+  },
+  {
+    "revision": "2721bd8da133696e04de69aabe3db9d4",
+    "url": "/MainSet1/static/media/pic4.2721bd8d.jpg"
+  },
+  {
+    "revision": "fdd6313f0daa2193b23fbb6f3ecebbec",
+    "url": "/MainSet1/static/media/pic4.fdd6313f.jpg"
+  },
+  {
+    "revision": "42db8d5de7be2e4de9f896c479b2b3b7",
+    "url": "/MainSet1/static/media/rondel.42db8d5d.svg"
+  },
+  {
+    "revision": "a2867013ac1fbc67c3857be13d2da002",
+    "url": "/MainSet1/static/media/set1.a2867013.jpg"
+  },
+  {
+    "revision": "77bfef938fb73036ced30f1d82ca2d2d",
+    "url": "/MainSet1/static/media/talerzyk.77bfef93.svg"
+  }
+]);
